6d8846a5c811e7aa5012ed359b53472421c33666
swar1999 <muzan22kris@gmail.com>
2023-10-16 12:16:21 -0700

    task 0

--- main.h
+++ main.h
@@ -3,5 +3,12 @@
 #include <unistd.h>
 #include <stdio.h>
 #include <stdarg.h>
+/* main body*/
 int _printf(const char *format, ...);
+/*specifiers functions*/
+int char_func(va_list sp);
+int string_func(va_list sp);
+int percent_func(va_list sp);
+/*help functions*/
+int _strlen(char *s);
 #endif
--- specifiers_functions.c
+++ specifiers_functions.c
@@ -15,9 +15,9 @@ int _strlen(char *s)
 	}
 	return (len);
 }
-/*
+/**
  * char_func - function to handle specifier 'c'
- * @type: variadic function argument
+ * @sp: variadic function argument
  * Return: charcter to be printed
  */
 int char_func(va_list sp)
@@ -25,9 +25,22 @@ int char_func(va_list sp)
 	char c = va_arg(sp, int);
 	return (write(1, &c, 1));/*return the chars*/
 }
+/**
+ * string_func - function to handle specifier 's'
+ * @sp: variadic function argument
+ * Return: string to be printed
+ */
 int string_func(va_list sp)
 {
 	char *s = va_arg(sp, char *);
 	return (write(1, s, strlen(s)));/*return string*/
 }
-
+/**
+ * percent_func - function to handle specifier '%'
+ * @sp:variadic function argument
+ * Return: character to be printed
+ */
+int percent_func(va_list sp)
+{
+	return(write(1, "%%", 1));
+}
