bb1a7c164014a98ded1062455900f341192158ed
swar1999 <muzan22kris@gmail.com>
2023-10-16 10:57:33 -0700

    printf function body

---
+++ my_prinft.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * _printf - function that do samr work as printf
+ * @formate: character
+ * Return: number of charactuers that been printed
+ */
+int _printf(const char *format, ...)
+{
+	int i = 0;/*counter*/
+	int counter = 0;/* to count each characture that is printed*/
+	va_list character;/*ceart variadic list*/
+	if (format == NULL)/*no character detected*/
+		return (-1);/*failed*/
+	va_start(character, format);/*initlize the list*/
+	while (format[i] != '\0')/*end of string*/
+	{
+		if (format[i] != '%')/*no specifiers*/
+			counter++;/*print string*/
+		else
+		{
+			/*should handle each caes*/
+		}
+		i++;
+	}
+	va_end(character);/*rlease the list*/
+	return (counter);/
