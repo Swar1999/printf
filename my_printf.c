fc267160592db2964974615f0b00a60c3e7a8654
swar1999 <muzan22kris@gmail.com>
2023-10-17 13:28:17 -0700

    task 0

--- my_printf.c
+++ my_printf.c
@@ -34,12 +34,7 @@ int _printf(const char *format, ...)
 					specifier = percent_func(args);
 					counter += specifier;
 				}
-				else if (format[i] == '%' && !format[i + 1])
-					return (-1);
-				else if (format[i] == '%' && format[i + 1] == ' ' && !format[i + 2])
-					return (-1);
 			}
-				i++;/*move next */
 		}
 		va_end(args);/*rlease the list*/
 		return (counter);
